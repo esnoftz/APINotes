e757ffed2f890c1ef5a0c7a3c4f328bbe552b57d
esnoftz <esnoftz@gmail.com>
2025-01-08 07:41:57 -0600

    Initial Commit

---
+++ APINotesTests/APINotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  APINotesTests.swift
+//  APINotesTests
+//
+//  Created by EVANGELINE NOFTZ on 1/8/25.
+//
+
+import Testing
+@testable import APINotes
+
+struct APINotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
