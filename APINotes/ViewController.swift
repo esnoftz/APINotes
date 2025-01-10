08c7c13edce780bc6aa11254c6c5638fb1253280
esnoftz <esnoftz@gmail.com>
2025-01-10 08:06:56 -0600

    Used date formatting.

--- APINotes/ViewController.swift
+++ APINotes/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tempOutlet: UILabel!
     
+    @IBOutlet weak var tempOutlet2: UILabel!
+    
+    @IBOutlet weak var tempOutlet3: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -47,6 +53,49 @@ class ViewController: UIViewController {
                                 }
                             }
                         }
+                        
+                        if let sysDictionary = jsonObj.value(forKey: "sys") as? NSDictionary {
+                            print(sysDictionary)
+                            if let theSunrise = sysDictionary.value(forKey: "sunrise") as? Int {
+                                let timeInterval = TimeInterval(theSunrise)
+                                let sunriseDate = Date(timeIntervalSince1970: timeInterval)
+                                print(sunriseDate)
+                                
+                                let formatter = DateFormatter()
+                                formatter.dateStyle = .none // we don't care about the date for sunrise time
+                                formatter.timeStyle = .short
+                                
+                                let sunriseFormatterDate = formatter.string(from: sunriseDate)
+                                print(sunriseFormatterDate)
+                                
+                                
+                                // happens on the main thread
+                                DispatchQueue.main.async {
+                                    self.tempOutlet2.text = "Sunrise is \(sunriseFormatterDate)"
+                                }
+                            }
+                            
+                            if let theSunset = sysDictionary.value(forKey: "sunset") as? Int {
+                                let timeInterval = TimeInterval(theSunset)
+                                let sunsetDate = Date(timeIntervalSince1970: timeInterval)
+                                print(sunsetDate)
+                                
+                                let formatter = DateFormatter()
+                                formatter.dateStyle = .none // we don't care about the date for sunrise time
+                                formatter.timeStyle = .short
+                                
+                                let sunsetFormatterDate = formatter.string(from: sunsetDate)
+                                print(sunsetFormatterDate)
+
+                                // happens on the main thread
+                                DispatchQueue.main.async {
+                                    self.tempOutlet3.text = "Sunset is \(sunsetFormatterDate)"
+                                }
+                            }
+
+                        }
+                        
+
                     } else {
                         print("Can't convert to json")
                     }
